2b941649789b39955a303610d3187c28f3638540
BaronLiu <baronliu1993@gmail.com>
2025-07-13 20:48:11 -0400

    frontend changes

--- src/app/components/home/navbar.js
+++ src/app/components/home/navbar.js
@@ -11,11 +11,14 @@ export default async function Navbar({ authorisedStatus }) {
           <Link href="/">Home</Link>
         </li>
         {authorisedStatus ? (
-          <div className = "flex space-x-6">
+          <div className="flex space-x-6">
             <li className="hover:font-semibold hover:bg-[#F8F8F8] p-1 rounded-md">
               <Link href="/">New QR Code</Link>
             </li>
             <LogoutButton />
+            <li className="hover:font-semibold hover:bg-[#F8F8F8] p-1 rounded-md">
+              <Link href="/dashboard">Dashboard</Link>
+            </li>
           </div>
         ) : (
           <li className="hover:font-semibold hover:bg-[#F8F8F8] p-1 rounded-md">
